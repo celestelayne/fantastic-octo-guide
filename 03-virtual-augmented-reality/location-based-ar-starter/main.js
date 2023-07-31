b41a5cbb3ff161776e348caab6387efc7cfd7956
Celeste Layne <layne.celeste@gmail.com>
2023-07-31 16:00:10 -0400

    update location based code snipped

--- 03-virtual-augmented-reality/location-based-ar-starter/main.js
+++ 03-virtual-augmented-reality/location-based-ar-starter/main.js
@@ -1,9 +1,31 @@
 console.log('this works');
 
+const btn = document.querySelector('button');
+const result = document.querySelector('#result');
+
 // callback function
 const getGPSLocation = event => {
     console.log("page is fully loaded");
+    // get the current position
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    } else {
+        console.log('not supported')
+    }
+    
+}
+
+// handle success
+const onSuccess = position => {
+    console.log(position)
+    const { latitude, longitude } = position.coords
+    result.textContent = `${latitude} , ${longitude}`
+}
+
+// handle success
+const onError = () => {
+    console.log('error')
 }
 
 // event listener
-window.addEventListener("load", getGPSLocation);
+btn.addEventListener("click", getGPSLocation);
